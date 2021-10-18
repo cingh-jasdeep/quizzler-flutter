2bc9fa8b4410ce248c88b368a149821fce6a8d18
Jasdeep Singh <cingh.jasdeep@gmail.com>
2021-10-18 13:10:14 +0530

    final

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText = '';
+  bool questionAnswer = true;
+
+  Question({required String q, required bool a}) {
+    this.questionText = q;
+    this.questionAnswer = a;
+  }
+}
