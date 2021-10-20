2b8616f96315b5340f9df821eb7ea6c3aa48bd7a
Jasdeep Singh <cingh.jasdeep@gmail.com>
2021-10-20 12:20:47 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -30,18 +31,31 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+
+  bool a = false;
+  List<Question> questionBank = [
+    Question(
+      q: 'You can lead a cow stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(
+      q: 'A slug\'s blood is green.',
+      a: true,
+    ),
+  ];
+
   /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [false, true, true];
+
   int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -54,7 +68,7 @@ question3: 'A slug\'s blood is green.', true,
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -81,9 +95,8 @@ question3: 'A slug\'s blood is green.', true,
               ),
               onPressed: () {
                 //The user picked true.
-
                 setState(() {
-                  if (answers[questionNumber] == true) {
+                  if (questionBank[questionNumber].questionAnswer == true) {
                     scoreKeeper.add(
                       Icon(
                         Icons.check,
@@ -121,7 +134,7 @@ question3: 'A slug\'s blood is green.', true,
               onPressed: () {
                 //The user picked false.
                 setState(() {
-                  if (answers[questionNumber] == false) {
+                  if (questionBank[questionNumber].questionAnswer == false) {
                     scoreKeeper.add(
                       Icon(
                         Icons.check,
